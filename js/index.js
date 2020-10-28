f892424f7348fbff3d87c0aa991d878a010bbf96
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-10-28 11:39:28 +0900

    .

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var testFunc = function testFunc() {
+  alert("Hello World");
+};
+
+testFunc();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+let testFunc = () => {
+    alert("Hello World");
+  }
+   
+  testFunc();
