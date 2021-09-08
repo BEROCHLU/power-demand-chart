f29b13f5cf59a774fbe23c51a2bf26a0901b39d4
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-09-08 11:07:38 +0900

    .

--- test/src4/src/jqtab.js
+++ test/src4/src/jqtab.js
@@ -2,14 +2,15 @@
 
 $('li[name="arrow"]').on('click', function () {
     const INDEX = $(this).index();
+    const LEN = $('li').length;
     //アクティブなクラスは全て削除
     $('li').removeClass('active-tab');
     $('#panel_group>div').removeClass('active-panel');
     //クリックした要素liにアクティブを追加
     //$(this).addClass('active-tab');
     $('li').eq(INDEX).addClass('active-tab');
-    $('li').eq(INDEX+5).addClass('active-tab');
-    $('#panel_group>div').eq(INDEX-1).addClass('active-panel');
+    $('li').eq(INDEX + LEN / 2).addClass('active-tab');
+    $('#panel_group>div').eq(INDEX - 1).addClass('active-panel');
     //$(`#panel_group>div:nth-of-type(${INDEX+1})`).addClass('active-panel');
     //console.log(INDEX);
     const n = Math.min(2, INDEX);
